b7ac902edfc6805614388af09933a7c15d2f6b1b
Andrei Shelenhouski <andrei_shelenhouski@epam.com>
2023-01-16 15:26:10 +0100

    feat: add firebase config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyBL_9ODJWl82lRHz9bH7M6ea8HIKn9flzQ',
+    authDomain: 'rs-school-feed.firebaseapp.com',
+    projectId: 'rs-school-feed',
+    storageBucket: 'rs-school-feed.appspot.com',
+    messagingSenderId: '573642716881',
+    appId: '1:573642716881:web:3de971d78bcbf05dce77f0',
+    measurementId: 'G-EPERRT0LFM',
+    locationId: 'europe-west',
+  },
+  production: true,
+  env: 'production',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyCS6ogUrkmJ02uuQfdSnOepCNmURf-DtmI',
+    authDomain: 'rs-school-feed-stage.firebaseapp.com',
+    projectId: 'rs-school-feed-stage',
+    storageBucket: 'rs-school-feed-stage.appspot.com',
+    messagingSenderId: '567420674620',
+    appId: '1:567420674620:web:7e144a6d04c6af1a4ad939',
+    measurementId: 'G-946RTNMW73',
+    locationId: 'europe-west',
+  },
+  production: false,
+  env: 'development',
+};
